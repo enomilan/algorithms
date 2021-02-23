89564f422ca794c492b27b6ea3459a86abf126a6
enomilan <andrewmusta@gmail.com>
2021-02-23 00:53:05 -0600

    2nd commit

--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -1,7 +1,18 @@
 // Implement Insertion Sort
 
 function insertionSort(list) {
+  for (let i = 1 ; i < list.length; i++) {
+    for (let j = i; j > 0; j--) {
+      
+      if (list[j] < list[j-1]) {
+      let temp = list[j]
+      list[j] = list[j - 1]
+      list [j -1] = temp
 
+
+        }
+    }
+  }
 
 
 }
