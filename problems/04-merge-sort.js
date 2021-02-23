3d0cd9c8e699306aa6f12311e5f9fbed2c737ee6
enomilan <andrewmusta@gmail.com>
2021-02-23 01:44:04 -0600

    final commit

--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -1,11 +1,38 @@
 // Implement Merge Sort
 
 function merge(array1, array2) {
-  // your code here
+  let merge =[]
+  let leftIndex = 0
+  let rightIndex = 0
+
+  while (leftIndex < array1.length && rightIndex < array2.length) {
+    let leftEl = array1[leftIndex]
+    let rightEl = array2[rightIndex]
+
+    if (leftEl < rightEl) {
+      merge.push(leftEl)
+      leftIndex++
+    } else {
+      merge.push(rightEl)
+      rightIndex++
+    }
+
+  } 
+  return [...merge, ...array1.slice(leftIndex), ...array2.slice(rightIndex)]
 }
 
 function mergeSort(array) {
-  // your code here
+  if (array.length <= 1) return array
+  
+  let midIndex = Math.floor(array.length/2)
+  let left = array.slice(0, midIndex)
+  let right = array.slice(midIndex)
+
+  let leftSort = mergeSort(left)
+  let rightSort = mergeSort(right)
+
+  return merge(leftSort, rightSort)
+  
 }
 
 module.exports = {
--- problems/05-quick-sort.js
+++ problems/05-quick-sort.js
@@ -1,8 +1,20 @@
 // Implement Quick Sort
 
 function quickSort(array) {
-  // your code here
+  if (array.length <= 1) return array
+
+
+let pivot = array.shift();
+let left = array.filter(el => el < pivot);
+let right = array.filter(el => el >= pivot);
+
+let leftSorted = quickSort(left);
+let rightSorted = quickSort(right);
+
+return [ ...leftSorted, pivot, ...rightSorted ];
+
 }
+
 
 
 module.exports = {
